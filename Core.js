1c398cc7b6e90c413fef82c290f3d9f7fd677538
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2022-12-22 15:58:19 +0500

    Update Core.js

--- Core.js
+++ Core.js
@@ -4133,7 +4133,7 @@ case '1wallpaper' :
     if (isBanChat) return reply(mess.bangc)
 	if (!args.join(" ")) return replay("Example -1wallpaper Pubg")
     reply(mess.waiting)
-     waifudhgd = await getBuffer(`https://api.lolhuman.xyz/api/wallpaper?apikey=${global.lolhuman}&query=${args.join(" ")`)     
+     waifudhgd = await getBuffer(`https://api.lolhuman.xyz/api/wallpaper?apikey=${global.lolhuman}&query=${args.join(" ")}`)     
      let nsfwapireply = [
         {buttonId: `${prefix}${command}`, buttonText: {displayText: `>>`}, type: 1},
         ]
